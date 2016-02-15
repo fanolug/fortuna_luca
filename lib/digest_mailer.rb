8d678c67682ca0922368cb9f5c14f4e79a9651a4
Alessandro Lepore <lepore.ale@gmail.com>
2016-02-15 10:08:06 +0100

    update digest mailer

--- lib/digest_mailer.rb
+++ lib/digest_mailer.rb
@@ -22,8 +22,8 @@ class DigestMailer
     mail = Mail.new
     mail.delivery_method(:smtp, smtp_config)
     mail.from ='Fortuna Luca <fortuna.luca@fanolug.org>'
-    mail.to = 'a.lepore@freegoweb.it'
-    mail.subject '[i link della settimana]'
+    mail.to = 'fanolug@googlegroups.com'
+    mail.subject "[i link della settimana] #{Date.today.strftime("%d/%m")}"
     mail.body = content
     mail.deliver! if content
   end
@@ -46,10 +46,11 @@ class DigestMailer
     return if tweets.none?
 
     tweets.reverse.map do |tweet|
+      # rewrite short urls as full urls
       rewrite_entities(tweet.text.dup, tweet.urls) do |entity, _|
         entity.expanded_url
       end
-    end.join("\n\n").gsub(' #ilds ', ' ') # temporary
+    end.join("\n\n")
   end
 
   def tweets_from_last_week
@@ -59,7 +60,7 @@ class DigestMailer
     tweets.take_while do |tweet|
       tweet.created_at.to_datetime >= DateTime.now - 7 # 1 week ago
     end.find_all do |tweet|
-      tweet.text =~ /.+\[.+\]$/
+      tweet.text =~ /.+\[.+\]$/ # match common pattern
     end
   end
 end
