ef3bb09561f7a7df98238813bc805f1db2869111
trottomv <trottomv@gmail.com>
2016-05-28 09:49:48 +0200

    comics autopost update

--- config/schedule.rb
+++ config/schedule.rb
@@ -9,10 +9,14 @@ every :hour, at: 15 do
   runner 'Calendar.new.notify'
 end
 
-every :day, at: '10:30am' do
+every :day, at: '10:00am' do
   runner 'Comics.new.notifyxkcd'
 end
 
-every '05 6-23 * * *' do
+every :day, at: '13:00am' do
+  runner 'Comics.new.notifytous'
+end
+
+every :day, at: '18:00am' do
   runner 'Comics.new.notifytous'
 end
--- lib/comics.rb
+++ lib/comics.rb
@@ -27,14 +27,27 @@ class Comics
 
   def notifytous
     rss = SimpleRSS.parse open('http://turnoff.us/feed.xml')
-    timefeed = rss.items.first.pubDate.strftime("%d-%m-%Y %H")
-    prev_hour = (Time.now - 3600).strftime("%d-%m-%Y %H")
+    timefeed = rss.items.first.pubDate.strftime("%d-%m-%Y")
+    current_day = Time.now.strftime("%d-%m-%Y")
 
-    if timefeed == prev_hour
+    if timefeed == current_day
       telegram_client.api.send_message(chat_id: ENV['TELEGRAM_CHAT_ID'], text: rss.items.first.title)
       logger.info "Comics#notifytous run with work"
     else
       logger.info "Comics#notifytous run"
+    end
+  end
+
+  def notifycommitstrip
+    rss = SimpleRSS.parse open('http://www.commitstrip.com/en/feed/')
+    timefeed = rss.items.first.pubDate.strftime("%d-%m-%Y")
+    current_day = Time.now.strftime("%d-%m-%Y")
+
+    if timefeed == current_day
+      telegram_client.api.send_message(chat_id: ENV['TELEGRAM_CHAT_ID'], text: rss.items.first.title)
+      logger.info "Comics#notifycommitstrip run with work"
+    else
+      logger.info "Comics#notifycommitstrip run"
     end
   end
 
