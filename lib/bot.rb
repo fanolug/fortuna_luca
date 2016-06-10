08e108e0ac3ced8472a623e56c3e1d325aae1eb0
Alessandro Lepore <lepore.ale@gmail.com>
2016-06-10 12:52:08 +0200

    refactor Bot with public #send_message

--- lib/bot.rb
+++ lib/bot.rb
@@ -4,23 +4,15 @@ require 'twitter'
 require 'xkcd'
 
 class Bot
-  attr_reader :name, :telegram_client, :twitter_client, :logger
+  attr_reader :logger
 
   def initialize
     Dotenv.load
-    @name = 'Fortuna Luca telegram bot'
     @logger = Logger.new('log/production.log')
-
-    telegram_client_init
-    @twitter_client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
-      config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
-      config.access_token        = ENV['TWITTER_TOKEN']
-      config.access_token_secret = ENV['TWITTER_TOKEN_SECRET']
-    end
   end
 
   def run!
+    name = 'Fortuna Luca telegram bot'
     Process.setproctitle(name)
     Process.daemon(true, true)
     logger.info "Running as '#{name}', pid #{Process.pid}"
@@ -37,32 +29,32 @@ class Bot
             when /^\/ilinkdellasettimana (.+)/
               tweet!(message, $1)
             when /^\/(xkcd|comics)/
-              respond(message.chat.id, XKCD.img)
+              send_message(message.chat.id, XKCD.img)
             when /^\/meteops/
-              respond(message.chat.id, "http://trottomv.dtdns.net/meteo#{Time.now.strftime("%Y%m%d")}.png")
+              send_message(message.chat.id, "http://trottomv.dtdns.net/meteo#{Time.now.strftime("%Y%m%d")}.png")
             end
           end
         end
       rescue Telegram::Bot::Exceptions::ResponseError => exception
         logger.error "#{exception.message} (#run)"
         raise if exception.error_code == '409'
-        telegram_client_init
+        @telegram_client = nil # reconnect
       end
+    end
+  end
+
+  def send_message(chat_id = ENV['TELEGRAM_CHAT_ID'], text)
+    begin
+      telegram_client.api.send_message(chat_id: chat_id, text: text)
+    rescue Telegram::Bot::Exceptions::ResponseError => exception
+      logger.error "#{exception.message} (#send_message chat_id: #{chat_id}, text: #{text})"
     end
   end
 
   private
 
-  def respond(chat_id, text)
-    begin
-      telegram_client.api.send_message(chat_id: chat_id, text: text)
-    rescue Telegram::Bot::Exceptions::ResponseError => exception
-      logger.error "#{exception.message} (#respond chat_id: #{chat_id}, text: #{text})"
-    end
-  end
-
   def send_help(message)
-    respond(message.from.id, "Usage:\n/ilinkdellasettimana <descrizione, link eccetera> - Posta su Twitter")
+    send_message(message.from.id, "Usage:\n/ilinkdellasettimana <descrizione, link eccetera> - Posta su Twitter")
   end
 
   def tweet!(message, text)
@@ -71,7 +63,7 @@ class Bot
     sender = message.from
     text = "#{text} [#{sender.username}]"
     tweet = twitter_client.update(text)
-    respond(sender.id, tweet.url.to_s)
+    send_message(sender.id, tweet.url.to_s)
   end
 
   def validate(message, text)
@@ -97,7 +89,16 @@ class Bot
     errors.none?
   end
 
-  def telegram_client_init
-    @telegram_client = Telegram::Bot::Client.new(ENV['TELEGRAM_TOKEN'])
+  def telegram_client
+    @telegram_client ||= Telegram::Bot::Client.new(ENV['TELEGRAM_TOKEN'])
+  end
+
+  def twitter_client
+    @twitter_client ||= Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+      config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+      config.access_token        = ENV['TWITTER_TOKEN']
+      config.access_token_secret = ENV['TWITTER_TOKEN_SECRET']
+    end
   end
 end
