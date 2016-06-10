b3e593e562dd74a2fc7b466b0de3526a372dbea0
Alessandro Lepore <lepore.ale@gmail.com>
2016-06-10 14:28:34 +0200

    parse specific feeds exceptions for nice chat results

--- lib/rss_reader.rb
+++ lib/rss_reader.rb
@@ -15,6 +15,22 @@ class RssReader
   def items_for_last_minutes(minutes)
     items.take_while do |item|
       Time.now.utc - item.pubDate.utc <= minutes * 60
+    end.each do |item|
+      item[:chat_preview] = chat_preview_for(item)
+    end
+  end
+
+  private
+
+  def chat_preview_for(item)
+    if item.link =~ /turnoff.us/
+      item.link.sub('/geek/', '/image/en/') + '.png'
+    elsif item.link =~ /commitstrip.com/
+      URI.extract(item.content_encoded)
+    elsif item.link =~ /doodle.com\/create/
+      item.title
+    else
+      item.link
     end
   end
 
