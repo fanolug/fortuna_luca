c57f9ff0591bf4f652098871b270a13e1daf00b3
Alessandro Lepore <lepore.ale@gmail.com>
2017-09-27 21:41:10 +0200

    Fix default command handling

--- lib/bot.rb
+++ lib/bot.rb
@@ -63,8 +63,9 @@ class Bot
       send_message(message.chat.id, WebSearcher.new($1).first_link)
     when /^\/meteops/i
       send_message(message.chat.id, "http://trottomv.suroot.com/meteo#{Time.now.strftime("%Y%m%d")}.png")
-    # try AI to generate some response
+    # default: try AI to generate some response
     else
+      text = text.sub(/\A\/\S* /, "") # remove /command part
       send_message(message.chat.id, AI::Responder.new(text).call)
     end
   end
