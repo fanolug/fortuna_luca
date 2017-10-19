2680eafe596b0f99c849ba59f44265584a12237f
Matteo Vitali <trottomv@gmail.com>
2017-10-19 20:26:48 +0200

    remove parse_mode for markdown

--- lib/telegram_client.rb
+++ lib/telegram_client.rb
@@ -1,14 +1,14 @@
 require 'telegram/bot'
 
 module TelegramClient
-  def send_message(chat_id, text, parse_mode)
+  def send_message(chat_id, text)
     if text.to_s == ""
       logger.info "Blank message text (#send_message)"
       return
     end
 
     begin
-      telegram_client.api.send_message(chat_id: chat_id, text: text, parse_mode: parse_mode)
+      telegram_client.api.send_message(chat_id: chat_id, text: text)
     rescue Telegram::Bot::Exceptions::ResponseError => exception
       logger.error "#{exception.message} (#send_message chat_id: #{chat_id}, text: #{text})"
     end
