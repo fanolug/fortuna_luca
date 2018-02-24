ddf6cdae216a4a5a08b82c79b26e9edd090ddc3e
Matteo Vitali <trottomv@gmail.com>
2018-02-24 12:48:29 +0100

    test facebook

--- test/test_facebook_reader.rb
+++ test/test_facebook_reader.rb
@@ -1,12 +1,14 @@
+require "dotenv"
 require_relative "test_helper"
 require_relative "../lib/facebook_reader"
 
 describe FacebookReader do
   before do
+    Dotenv.load
     @facebook_reader = FacebookReader.new("fanolug")
   end
 
-  describe "#posts" do
+  describe "#koala" do
     it "sends the api request" do
       Koala::Facebook::API.any_instance
     end
