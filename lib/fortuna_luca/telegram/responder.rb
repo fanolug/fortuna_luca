4efd8b937dd2519220cb4bdee9722eab3118b091
Alessandro Lepore <lepore.ale@gmail.com>
2019-11-18 08:57:30 +0100

    Add telegram responder class
    using the previous code

---
+++ lib/fortuna_luca/telegram/responder.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require 'dotenv'
+require_relative '../../xkcd'
+require_relative '../../telegram_client'
+require_relative '../../ai/responder'
+
+module FortunaLuca
+  module Telegram
+    class Responder
+      include TelegramClient
+
+      # @param message [Telegram::Bot::Types::Message] The Telegram message
+      def initialize(message)
+        Dotenv.load
+        @message = message
+      end
+
+      def call
+        # clean up text
+        text = message.text.to_s.tr("\n", ' ').squeeze(' ').strip
+
+        case text
+        # display a XKCD comic
+        when /^\/(xkcd|comics)/i # /xkcd or /comics
+          send_message(chat_id, Xkcd.new.random_image)
+        # get the first web search result link
+        when /^\/google (.+)/i # /google
+          send_message(chat_id, WebSearcher.new($1).first_link)
+        # default: try AI to generate some response
+        else
+          text = text.sub(/\A\/\S* /, "") # remove /command part
+          send_message(chat_id, AI::Responder.new(text).call)
+        end
+
+        true
+      end
+
+      private
+
+      attr_reader :message
+
+      def chat_id
+        message.chat.id
+      end
+    end
+  end
+end
