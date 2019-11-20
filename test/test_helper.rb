38dc7c1ea928dab1a985c21602b5a702fa5a80b8
Alessandro Lepore <lepore.ale@gmail.com>
2019-11-20 18:57:37 +0100

    Add webhook server tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,9 @@
 require "minitest/autorun"
 require "mocha/mini_test"
 require "minitest/reporters"
+require "rack/test"
+
+ENV["RACK_ENV"] = "test"
+ENV["SECRET_WEBHOOK_PATH"] = "/the-secret-path"
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
