360b3337ca8dbb7201760a154bca4002062f81c6
Alessandro Lepore <lepore.ale@gmail.com>
2019-11-22 12:40:26 +0100

    Add Xkcd class test

---
+++ test/test_xkcd.rb
@@ -0,0 +1,20 @@
+require_relative "test_helper"
+require_relative "../lib/xkcd"
+
+describe Xkcd do
+  let(:instance) { Xkcd.new }
+
+  describe '#random_image' do
+    let(:index_response) { '{"num": 2231}' }
+    let(:detail_response) { '{"alt": "the alt", "img": "http://img.example.com"}' }
+
+    before do
+      stub_request(:get, "https://xkcd.com/info.0.json").to_return(body: index_response)
+      stub_request(:get, %r{https://xkcd.com/\d+/info.0.json}).to_return(body: detail_response)
+    end
+
+    it 'returns the correct data' do
+      instance.random_image.must_equal("the alt: http://img.example.com")
+    end
+  end
+end
