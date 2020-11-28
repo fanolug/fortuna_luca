49f8492c9e4384363f666c13adc4235c0472ae98
Alessandro Lepore <lepore.ale@gmail.com>
2020-11-28 16:56:11 +0100

    Add a Wikipedia summary extractor class

---
+++ lib/fortuna_luca/wikipedia/summary.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require "httpclient"
+require "json"
+require_relative "../../logging"
+
+module FortunaLuca
+  module Wikipedia
+    class Summary
+      include Logging
+
+      # @param language [String] A valid Wikipedia language code
+      def initialize(language = "it")
+        @api_url = "https://#{language}.wikipedia.org/w/api.php"
+      end
+
+      # @param term [String] The term to search
+      # @return [String,nil] A short description, if found
+      def call(term)
+        page = data(term)&.dig("query", "pages")&.min_by { |_, v| v["index"] }
+        extract = page.dig(1, "extract") if page
+        extract.split(". ").first.split(".\n").first if extract
+      end
+
+      private
+
+      attr_reader :api_url
+
+      def data(term)
+        params = {
+          "action" => "query",
+          "generator" => "prefixsearch",
+          "gpssearch" => term,
+          "prop" => "extracts",
+          "exintro" => 1,
+          "redirects" => 1,
+          "explaintext" => 1,
+          "format" => "json"
+        }
+        result = HTTPClient.get(api_url, params)
+        JSON.parse(result.body)
+      rescue JSON::ParserError => e
+        logger.error "Wikipedia JSON parse error: #{e.message}"
+        nil
+      end
+    end
+  end
+end
---
+++ test/fortuna_luca/wikipedia/test_summary.rb
@@ -0,0 +1,44 @@
+require_relative "../../test_helper"
+require_relative "../../../lib/fortuna_luca/wikipedia/summary"
+
+describe FortunaLuca::Wikipedia::Summary do
+  let(:instance) { FortunaLuca::Wikipedia::Summary.new("it") }
+  let(:stubbed_response) { HTTP::Message.new_response(data) }
+
+  describe "#call" do
+    before do
+      HTTPClient.stubs(:get).with(
+        "https://it.wikipedia.org/w/api.php",
+        kind_of(Hash)
+      ).returns(stubbed_response)
+    end
+
+    describe "with valid data returned" do
+      let(:data) do
+        File.read(File.dirname(__FILE__) + '/../../fixtures/wikipedia_linux.json')
+      end
+
+      it "returns the Wikipedia summary" do
+        instance.call("linux").must_equal(
+          "Linux (/ˈlinuks/, pronuncia inglese [ˈlɪnʊks]) è una famiglia di sistemi operativi di tipo Unix-like, pubblicati in varie distribuzioni, aventi la caratteristica comune di utilizzare come nucleo il kernel Linux: oggi molte importanti società nel campo dell'informatica come Google, IBM, Oracle Corporation, Hewlett-Packard, Red Hat, Canonical, Novell e Valve sviluppano e pubblicano sistemi Linux."
+        )
+      end
+    end
+
+    describe "with invalid JSON data returned" do
+      let(:data) { "" }
+
+      it "returns nil" do
+        instance.call("linux").must_equal(nil)
+      end
+    end
+
+    describe "with blank data returned" do
+      let(:data) { '{"batchcomplete":""}' }
+
+      it "returns nil" do
+        instance.call("linux").must_equal(nil)
+      end
+    end
+  end
+end
