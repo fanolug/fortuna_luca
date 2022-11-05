69838f13bb3cdeab631af90c64724ac5a2d82961
Alessandro Lepore <lepore.ale@gmail.com>
2022-11-05 08:58:31 +0100

    Remove digest code
    Not used anymore

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,10 +1,6 @@
 set :output, '/var/www/fortuna_luca/shared/log/cron.log'
 job_type :bin, 'cd :path && bundle exec ruby ./bin/:task :output'
 
-every :monday, at: '9am' do
-  bin :send_digest
-end
-
 every 1.hours do
   bin :send_last_tweets_media
 end
