d4b08b8f34773151a1f52c23400d4dcbfbe7e4b1
Alessandro Lepore <lepore.ale@gmail.com>
2023-02-11 16:30:10 +0100

    Add CO2 Airthings support

--- lib/fortuna_luca/airthings/status.rb
+++ lib/fortuna_luca/airthings/status.rb
@@ -9,26 +9,32 @@ module FortunaLuca
     module Status
       include FortunaLuca::Telegram::Client
 
-      KEYS = %w[temp humidity radonShortTermAvg voc mold].freeze
+      # order matters
+      SUPPORTED_KEYS = %w[temp humidity co2 radonShortTermAvg voc mold].freeze
       UNITS = {
-        "temp" => "°",
+        "co2" => "ppm",
         "humidity" => "%",
+        "mold" => "/10",
         "radonShortTermAvg" => " Bq/m³",
+        "temp" => "°",
         "voc" => " ppb",
-        "mold" => "/10"
       }.freeze
       RANGES = {
-        "humidity" => { low: 30, high: 55, highest: 70 },
-        "radonShortTermAvg" => { low: 0, high: 100, highest: 150 },
-        "voc" => { low: 0, high: 250, highest: 2000 },
+        "co2" => { green: 0, yellow: 1000, red: 2000 },
+        "humidity" => { green: 30, yellow: 55, red: 70 },
+        "radonShortTermAvg" => { green: 0, yellow: 100, red: 150 },
+        "voc" => { green: 0, yellow: 250, red: 2000 },
       }.freeze
 
       # @return [String] Human-readabale status of all the sensors
       def airthings_status
         data = airthings_client.samples
         longest_room_name = data.map { |room, _| room.size }.max
+
         data.map do |room, samples|
-          formatted_samples = samples.slice(*KEYS).map do |key, value|
+          name_padding = " " * (longest_room_name - room.size)
+
+          formatted_samples = samples.slice(*SUPPORTED_KEYS).map do |key, value|
             formatted_value = "#{"%g" % value}#{UNITS[key]}"
             [
               I18n.t(key, scope: :airthings),
@@ -36,9 +42,9 @@ module FortunaLuca
               icon(key, value)
             ].join(" ").strip
           end.join(", ")
-          padding = " " * (longest_room_name - room.size)
 
-          "<pre>#{room}:#{padding} #{formatted_samples}</pre>"
+
+          "<pre>#{room}:#{name_padding} #{formatted_samples}</pre>"
         end.join("\n")
       end
 
@@ -53,10 +59,10 @@ module FortunaLuca
         return unless ranges
 
         case value
-        when -Float::INFINITY..ranges[:low] then "\u{1F7E1}"
-        when ranges[:low]..ranges[:high] then "\u{1F7E2}"
-        when ranges[:high]..ranges[:highest] then "\u{1F7E1}"
-        when ranges[:highest]..Float::INFINITY then "\u{1F534}"
+          when -Float::INFINITY..ranges[:green] then "\u{1F7E1}"
+          when ranges[:green]..ranges[:yellow] then "\u{1F7E2}"
+          when ranges[:yellow]..ranges[:red] then "\u{1F7E1}"
+          else "\u{1F534}"
         end
       end
     end
