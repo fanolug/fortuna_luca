11bb75f3b5a12184cc3ca1a3d7cd5d4d6e68263f
Alessandro Lepore <lepore.ale@gmail.com>
2023-06-01 23:43:32 +0200

    Add dall-e client

---
+++ lib/fortuna_luca/openai/dall_e_client.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require_relative "chat_gpt_client"
+
+module FortunaLuca
+  module OpenAI
+    class DallEClient < ChatGPTClient
+      # @param content [String] The input
+      # @return [String] The response
+      def call(content)
+        response = get_response(content)
+        if error = response.dig("error", "message")
+          logger.error(error)
+        else
+          response.dig("data", 0, "url")
+        end
+      end
+
+      private
+
+      def get_response(content)
+        client.images.generate(parameters: { prompt: content })
+      end
+    end
+  end
+end
